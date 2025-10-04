18e4f7f2231ebcf6623c59ec55d7e34b65dfd2de
Tomas McGuinness <tomas@tomasmcguinness.com>
2025-10-04 08:07:57 +0100

    Delete the frontend directory contents as there is nothing there.

--- Frontend/global.ts
+++
@@ -1,8 +0,0 @@
-import { CommissioningController } from "@project-chip/matter.js";
-
-declare global {
-    var matterServer : CommissioningController;
-    var energyManager: any
-}
-
-export default global;
